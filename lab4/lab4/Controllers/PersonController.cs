3cd5f7e43ca5c4f8a36b4272de070b8ed84da890
troyhagen8 <troyhagen8@gmail.com>
2017-10-12 16:30:03 -0400

    FINALLY THE PERSON WORKS!!!!!!

--- lab4/lab4/Controllers/PersonController.cs
+++ lab4/lab4/Controllers/PersonController.cs
@@ -9,7 +9,7 @@ namespace lab4.Controllers
 {
     public class PersonController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Person()
         {
             Person p = new Person
             {
@@ -21,7 +21,7 @@ namespace lab4.Controllers
             };
 
 
-            return View();
+            return View(p);
         }
     }
 }
